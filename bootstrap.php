d26ee971079f01407e13f2a4e3b3ca32cc320fb1
aulisius <aulisius7@gmail.com>
2022-06-24 17:13:54 +0530

    Disable loading since bluehost plugin doesn't support it

--- bootstrap.php
+++ bootstrap.php
@@ -1,28 +1,27 @@
 <?php
 
-use NewfoldLabs\WP\ModuleLoader\Container;
+// use NewfoldLabs\WP\ModuleLoader\Container;
 use NewfoldLabs\WP\Module\ECommerce\ECommerce;
 
-use function NewfoldLabs\WP\ModuleLoader\register;
+// use function NewfoldLabs\WP\ModuleLoader\register;
 
 if ( function_exists( 'add_action' ) ) {
-
-	add_action(
-		'plugins_loaded',
-		function () {
-			register(
-				[
-					'name'     => 'ecommerce',
-					'label'    => __( 'eCommerce', 'newfold-module-ecommerce' ),
-					'callback' => function ( Container $container ) {
-						new ECommerce( $container );
-					},
-					'isActive' => true,
-					'isHidden' => true,
-				]
-			);
-
-		}
-	);
+	// Current bluehost plugin doesn't support module loader
+	// add_action(
+	// 	'plugins_loaded',
+	// 	function () {
+	// 		register(
+	// 			[
+	// 				'name'     => 'ecommerce',
+	// 				'label'    => __( 'eCommerce', 'newfold-module-ecommerce' ),
+	// 				'callback' => function ( Container $container ) {
+	// 					new ECommerce( $container );
+	// 				},
+	// 				'isActive' => true,
+	// 				'isHidden' => true,
+	// 			]
+	// 		);
+	// 	}
+	// );
 
 }
