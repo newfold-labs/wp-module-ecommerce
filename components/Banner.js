444588121dd9026641b603cf27718beea3627bc5
aulisius <aulisius7@gmail.com>
2022-06-29 12:13:04 +0530

    Simplify Banner and setup ManageProducts

--- components/Banner.js
+++ components/Banner.js
@@ -3,24 +3,12 @@ import { ReactComponent as WIP } from "./WIP.svg";
 
 const BannerConfigurationByStatus = {
   wip: {
-    vars: {
-      "--nfd-ecommerce-status": "rgba(229, 22, 7, 1)",
-      "--nfd-ecommerce-status-border": "var(--nfd-ecommerce-border-primary)",
-      "--nfd-ecommerce-status-outer": "rgba(229, 22, 7, 0.2)",
-    },
     Icon: WIP,
     message: "Congrats on your new store! Let's get it ready to launch!",
-    status: "Coming Soon",
   },
   complete: {
-    vars: {
-      "--nfd-ecommerce-status": "rgba(23, 178, 18, 1)",
-      "--nfd-ecommerce-status-outer": "rgba(23, 178, 18, 0.2)",
-      "--nfd-ecommerce-status-border": "var(--nfd-ecommerce-status-outer)",
-    },
     Icon: Complete,
     message: "Your store is online and ready for business!",
-    status: "Live",
   },
 };
 
@@ -29,20 +17,11 @@ export function Banner({ state }) {
     ? BannerConfigurationByStatus.wip
     : BannerConfigurationByStatus.complete;
   return (
-    <div className="nfd-ecommerce-banner" style={BannerConfiguration.vars}>
+    <div className="nfd-ecommerce-banner">
       <div style={{ margin: "32px 0 22px" }}>
         <BannerConfiguration.Icon />
       </div>
-      <h1 className="heading accent">{BannerConfiguration.message}</h1>
-      <div className="status-bar">
-        <div className="status-icon" />
-        <span>
-          Store status:{" "}
-          <span style={{ color: "var(--nfd-ecommerce-status)" }}>
-            {BannerConfiguration.status}
-          </span>
-        </span>
-      </div>
+      <h1 className="accent">{BannerConfiguration.message}</h1>
       {state.wp.comingSoon ? (
         <span className="text status-notice">
           Your site is currently displaying a "Coming Soon" page. Once you are
--- components/ManageProducts.js
+++ components/ManageProducts.js
@@ -1,12 +1,51 @@
+import { ReactComponent as AddProducts } from "../icons/add-products.svg";
+import { ReactComponent as ImportProducts } from "../icons/import-products.svg";
+import { ReactComponent as Help } from "../icons/help.svg";
+import { Card } from "./Card";
 import { DashboardContent } from "./DashboardContent";
 
 export function ManageProducts(props) {
   return (
-    <DashboardContent
-      title="Add Products"
-      subtitle="Come here to manage your products or add new ones to your store."
-    >
-      <div />
-    </DashboardContent>
+    <>
+      <DashboardContent
+        title="Add Products"
+        subtitle="Come here to manage your products or add new ones to your store."
+      >
+        <div className="nfd-ecommerce-standard-actions-container">
+          <Card
+            variant="standard"
+            title="Add Products"
+            action="Add Info"
+            href="/wp-admin/post-new.php?post_type=product"
+          >
+            <AddProducts />
+          </Card>
+          <Card
+            variant="standard"
+            title="Import Products"
+            action="Add Info"
+            href="/wp-admin/edit.php?post_type=product&page=product_importer"
+          >
+            <ImportProducts />
+          </Card>
+        </div>
+      </DashboardContent>
+      <div style={{ height: "40px" }} />
+      <DashboardContent
+        title="First time adding a product?"
+        subtitle="Read this helpful knowledge base article to understand how to add different products to your store."
+      >
+        <div className="nfd-ecommerce-minimal-tasks-container">
+          <Card
+            variant="minimal"
+            title="How to add products"
+            action="Learn More"
+            href="/wp-admin/edit.php?post_type=product&page=product_importer"
+          >
+            <Help />
+          </Card>
+        </div>
+      </DashboardContent>
+    </>
   );
 }
