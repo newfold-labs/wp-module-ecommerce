dbff18ce7ff21b34393c14493bacb26096da63f7
aulisius <aulisius7@gmail.com>
2022-06-29 13:43:13 +0530

    PRESS4-4 Add logic for isFetching, errors etc.

--- components/GeneralSettings.js
+++ components/GeneralSettings.js
@@ -32,7 +32,7 @@ const OnboardingSteps = {
     editAction: "Edit Settings",
     setupUrl: "/wp-admin/admin.php?page=wc-settings&tab=shipping",
     editUrl: "/wp-admin/admin.php?page=wc-settings&tab=shipping",
-    isSetupDone: (state) => state.shipping.length > 1,
+    isSetupDone: (state) => state.onboarding.isComplete,
     SetupIcon: Shipping,
   },
   tax: {
@@ -49,13 +49,25 @@ const OnboardingSteps = {
 export function GeneralSettings(props) {
   let { wpModules } = props;
   const fetcher = (path) => wpModules.apiFetch({ path });
-
-  let { data: onboardingResponse = [] } = useSWR(
+  let { data: onboardingResponse, error } = useSWR(
     "/wc-admin/onboarding/tasks?ids=setup",
     fetcher
   );
+  if (!onboardingResponse) {
+    return (
+      <div style={{ height: "100%", display: "grid", placeContent: "center" }}>
+        <div className="bwa-loader" />
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div style={{ height: "100%", display: "grid", placeContent: "center" }}>
+        <h2>There was an error while loading this information</h2>
+      </div>
+    );
+  }
   let onboardingSetup = onboardingResponse[0];
-  let { data: shipping = [] } = useSWR("/wc/v3/shipping/zones", fetcher);
   let onboarding = Object.fromEntries(
     (onboardingSetup ? onboardingSetup.tasks : []).map((task) => [
       task.id,
@@ -65,20 +77,13 @@ export function GeneralSettings(props) {
   let completedSteps = Object.entries(OnboardingSteps).filter((task) => {
     let stepKey = task[0];
     let stepProgress = task[1];
-    return stepProgress.isSetupDone({
-      onboarding: onboarding[stepKey] ?? {},
-      shipping,
-    });
+    return stepProgress.isSetupDone({ onboarding: onboarding[stepKey] ?? {} });
   });
   let incompleteSteps = Object.entries(OnboardingSteps).filter((task) => {
     let stepKey = task[0];
     let stepProgress = task[1];
-    return !stepProgress.isSetupDone({
-      onboarding: onboarding[stepKey] ?? {},
-      shipping,
-    });
+    return !stepProgress.isSetupDone({ onboarding: onboarding[stepKey] ?? {} });
   });
-  console.warn(onboarding, completedSteps, incompleteSteps);
   return (
     <>
       {incompleteSteps.length > 0 ? (
