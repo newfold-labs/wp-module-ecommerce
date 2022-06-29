f0389fe052bfda78f51b4d47fea3da7c6dc5c444
aulisius <aulisius7@gmail.com>
2022-06-29 18:15:21 +0530

    Banner is not availabe if store is active

--- components/Banner.js
+++ components/Banner.js
@@ -1,39 +1,23 @@
-import { ReactComponent as Complete } from "./Complete.svg";
 import { ReactComponent as WIP } from "./WIP.svg";
 
-const BannerConfigurationByStatus = {
-  wip: {
-    Icon: WIP,
-    message: "Congrats on your new store! Let's get it ready to launch!",
-  },
-  complete: {
-    Icon: Complete,
-    message: "Your store is online and ready for business!",
-  },
-};
-
 export function Banner({ state }) {
-  let BannerConfiguration = state.wp.comingSoon
-    ? BannerConfigurationByStatus.wip
-    : BannerConfigurationByStatus.complete;
+  if (state.wp.comingSoon === false) {
+    return null;
+  }
   return (
-    <div className="nfd-ecommerce-banner">
-      <div style={{ margin: "32px 0 22px" }}>
-        <BannerConfiguration.Icon />
-      </div>
-      <h1 className="accent">{BannerConfiguration.message}</h1>
-      {state.wp.comingSoon ? (
+    <>
+      <div className="nfd-ecommerce-banner">
+        <div style={{ margin: "32px 0 22px" }}>
+          <WIP />
+        </div>
+        <h1>Congrats on your new store! Let's get it ready to launch!</h1>
         <span className="text status-notice">
           Your site is currently displaying a "Coming Soon" page. Once you are
           ready, launch your site.
         </span>
-      ) : (
-        <span className="text status-notice">
-          Your site is live and visible to everyone. Not quite ready?{" "}
-          <span>Enable the "Coming Soon" mode.</span>
-        </span>
-      )}
+        <div style={{ height: "32px" }} />
+      </div>
       <div style={{ height: "32px" }} />
-    </div>
+    </>
   );
 }
