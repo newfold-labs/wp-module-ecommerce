133c444ecd00797789ccb13a2859e7bac27f1276
aulisius <aulisius7@gmail.com>
2022-07-14 20:39:25 +0530

    Move to maintained dts-cli

---
+++ dts.config.js
@@ -0,0 +1,8 @@
+const url = require("@rollup/plugin-url");
+const svgr = require("@svgr/rollup");
+module.exports = {
+  rollup(config, options) {
+    config.plugins = [url(), svgr(), ...config.plugins];
+    return config;
+  },
+};
