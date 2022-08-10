c851b5e0eb1f6ae2869cb1d3e3125f7770eb6a18
aulisius <aulisius7@gmail.com>
2022-08-10 20:15:48 +0530

    API contracts are moved to a single file

---
+++ src/services.js
@@ -0,0 +1,26 @@
+import apiFetch from "@wordpress/api-fetch";
+
+export const Endpoints = {
+  WP_SETTINGS: '/wp/v2/settings',
+  WC_ONBOARDING: '/wc-admin/onboarding/profile',
+  PLUGIN_INSTALL: '/newfold-onboarding/v1/plugins/install',
+};
+export async function fetchWPSettings() {
+  return apiFetch({ path: Endpoints.WP_SETTINGS });
+}
+
+export async function updateWPSettings(data) {
+  return apiFetch({ path: Endpoints.WP_SETTINGS, method: 'POST', data });
+}
+
+export async function updateWCOnboarding(data) {
+  return apiFetch({ path: Endpoints.WC_ONBOARDING, method: 'POST', data });
+}
+
+export async function queuePluginInstall(plugin) {
+  return apiFetch({
+    path: Endpoints.PLUGIN_INSTALL,
+    method: 'POST',
+    data: { plugin, activate: true, queue: true },
+  }).catch(error => {});
+}
