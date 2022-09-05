d3af8cc3c6dc98decd9afc2a066fa0cf5df26f28
Ashritha <ashritha.belghatta@newfold.com>
2022-09-05 21:55:59 +0530

    Handled Responsiveness for all screens

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,6 +6,13 @@ import { ManageProducts } from "./ManageProducts";
 import { SiteStatus } from "./SiteStatus";
 import { useOnboardingCleanup } from "./useOnboardingCleanup";
 import { useSetupYITHWonderTheme } from "./useSetupYITHWonderTheme";
+import {
+  __experimentalNavigation as Navigation,
+  __experimentalNavigationMenu as NavigationMenu,
+  __experimentalNavigationItem as NavigationItem,
+} from "@wordpress/components";
+import { useViewportMatch } from "@wordpress/compose";
+import { useState } from "@wordpress/element";
 
 function getStepName(stepKey, state) {
   switch (stepKey) {
@@ -35,39 +42,82 @@ const guideSteps = [
 ];
 
 export function Dashboard(props) {
+  const isLargeViewport = useViewportMatch("mobile");
+  const [activeTab, setActiveTab] = useState("dashboard-tabs");
   let { key, StepContent } =
     guideSteps.find((step) => step.key === props.section) ?? guideSteps[0];
   useSetupYITHWonderTheme();
   let isCleanUpInProgress = useOnboardingCleanup(props.plugins.token?.hash);
-  let addCurtain = props.plugins?.status?.woocommerce !== 'Active';
-  let className = `nfd-ecommerce-dashboard ${addCurtain ? 'disableDashboardContent' : ''}` ;
+  let addCurtain = props.plugins?.status?.woocommerce !== "Active";
+  let className = `nfd-ecommerce-dashboard ${
+    addCurtain ? "disableDashboardContent" : ""
+  }`;
+
   return (
     <div className={className}>
-      <nav
-        aria-label={__("Setup Guide", "wp-module-ecommerce")}
-        className="nfd-ecommerce-dashboard-menu"
-      >
-        {guideSteps.map((step) => (
-          <a
-            key={getStepName(step.key, props.state)}
-            data-active={key === step.key}
-            href={`#/home/store/${step.key}`}
-          >
-            <li>{getStepName(step.key, props.state)}</li>
-          </a>
-        ))}
-      </nav>
-      <div className="nfd-ecommerce-dashboard-content">
-        {isCleanUpInProgress ? (
-          <div
-            style={{ height: "100%", display: "grid", placeContent: "center" }}
-          >
-            <div className="bwa-loader" />
-          </div>
-        ) : (
-          <StepContent {...props} />
-        )}
-      </div>
+      {isLargeViewport ? (
+        <nav
+          aria-label={__("Setup Guide", "wp-module-ecommerce")}
+          className="nfd-ecommerce-dashboard-menu"
+        >
+          {guideSteps.map((step) => (
+            <a
+              key={getStepName(step.key, props.state)}
+              data-active={key === step.key}
+              href={`#/home/store/${step.key}`}
+            >
+              <li>{getStepName(step.key, props.state)}</li>
+            </a>
+          ))}
+        </nav>
+      ) : (
+        <Navigation
+          className="nf-onboarding__mobile"
+          activeMenu={activeTab}
+          onActivateMenu={setActiveTab}
+        >
+          <NavigationMenu menu="dashboard-tabs">
+            {guideSteps.map((tab) => (
+              <NavigationItem
+                key={tab.key}
+                title={getStepName(tab.key, props.state)}
+                navigateToMenu={tab.key}
+                href={`#/home/store/${tab.key}`}
+              />
+            ))}
+          </NavigationMenu>
+          {guideSteps.map((tab) => {
+            return (
+              <NavigationMenu
+                key={`${tab.key}-menu`}
+                menu={tab.key}
+                parentMenu="dashboard-tabs"
+              >
+                <NavigationItem>
+                  <tab.StepContent {...props} />
+                </NavigationItem>
+              </NavigationMenu>
+            );
+          })}
+        </Navigation>
+      )}
+      {isLargeViewport ? (
+        <div className="nfd-ecommerce-dashboard-content">
+          {isCleanUpInProgress ? (
+            <div
+              style={{
+                height: "100%",
+                display: "grid",
+                placeContent: "center",
+              }}
+            >
+              <div className="bwa-loader" />
+            </div>
+          ) : (
+            <StepContent {...props} />
+          )}
+        </div>
+      ) : null}
     </div>
   );
 }
