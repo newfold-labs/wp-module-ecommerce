d180998cd60f1fe8b74f168d2ad74de96cc8aec8
aulisius <aulisius7@gmail.com>
2022-09-07 20:41:41 +0530

    Run formatting

--- src/components/StoreAnalytics.js
+++ src/components/StoreAnalytics.js
@@ -1,35 +1,33 @@
-import { __ } from "@wordpress/i18n";
+import { __ } from '@wordpress/i18n';
 export function StoreAnalytics(props) {
-    if (props?.plugins?.status?.woocommerce !== "Active" || props?.state?.wp.comingSoon === true) {
-        return null;
-    }
-    let storeAnalyticsLink = `/wp-admin/admin.php?${new URLSearchParams({
-        page: "wc-admin",
-        path: "/analytics/overview",
-        return_to_nfd: "/home/store/"+props.section
-    })}`;
-    return (
-        <>
-            <div className="nfd-ecommerce-store-analytics">
-                <h2>
-                    {__(
-                        "Store Analytics",
-                        "wp-module-ecommerce"
-                    )}
-                </h2>
-                <div style={{height: "8px"}}/>
-                <span className="store-analytics-notice">
-                    {__(
-                        "Get a detailed view of all your store performance analytics via the WooCommerce Analytics page.",
-                        "wp-module-ecommerce"
-                    )}
-                </span>
-                <div style={{height: "16px"}}/>
-                <a href= {storeAnalyticsLink}>
-                    {__("View Analytics", "wp-module-ecommerce")}
-                </a>
-            </div>
-            <div style={{height: "32px"}}/>
-        </>
-    )
+  if (
+    props?.plugins?.status?.woocommerce !== 'Active' ||
+    props?.state?.wp.comingSoon === true
+  ) {
+    return null;
+  }
+  let storeAnalyticsLink = `admin.php?${new URLSearchParams({
+    page: 'wc-admin',
+    path: '/analytics/overview',
+    return_to_nfd: '/home/store/' + props.section,
+  })}`;
+  return (
+    <>
+      <div className='nfd-ecommerce-store-analytics'>
+        <h2>{__('Store Analytics', 'wp-module-ecommerce')}</h2>
+        <div style={{ height: '8px' }} />
+        <span className='store-analytics-notice'>
+          {__(
+            'Get a detailed view of all your store performance analytics via the WooCommerce Analytics page.',
+            'wp-module-ecommerce'
+          )}
+        </span>
+        <div style={{ height: '16px' }} />
+        <a href={storeAnalyticsLink}>
+          {__('View Analytics', 'wp-module-ecommerce')}
+        </a>
+      </div>
+      <div style={{ height: '32px' }} />
+    </>
+  );
 }
