9af885ea3aa333ac83979a5e8244ad739b152a00
​Faizaan <aulisius@users.noreply.github.com>
2022-10-07 08:14:53 +0530

    version bump (#34)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => '85f825e716a6d7983399');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n'), 'version' => 'fa89264ce3dab1969677');
