3adf6e59ebd429a3cabe16ddb2e601661fb043ba
aulisius <aulisius7@gmail.com>
2022-10-27 16:36:38 +0530

    shouldRender now also receives state during the rendering pass

--- src/components/useCardManager.js
+++ src/components/useCardManager.js
@@ -1,5 +1,5 @@
-import apiFetch from '@wordpress/api-fetch';
-import useSWR from 'swr';
+import apiFetch from "@wordpress/api-fetch";
+import useSWR from "swr";
 
 function createDependencyTree(config) {
   return config
@@ -30,7 +30,9 @@ function useLoadDependencies(tree) {
   });
   async function onRefresh(dependency) {
     // TODO: Add Checks for if path is not found.
-    let [path] = Object.entries(tree).find(([, deps]) => deps.includes(dependency));
+    let [path] = Object.entries(tree).find(([, deps]) =>
+      deps.includes(dependency)
+    );
     let updatedResponse = await apiFetch({ path });
     await mutate(
       (realisedTree) => ({ ...realisedTree, [path]: updatedResponse }),
@@ -54,15 +56,17 @@ function extractDependencies(realisedTree, cardConfig) {
 export const useCardManager = (config) => {
   const tree = createDependencyTree(config);
   let { realisedTree, onRefresh } = useLoadDependencies(tree);
-  return config.filter(cardConfig => cardConfig.shouldRender()).map((cardConfig) => {
-    let { state: stateDefinition } = cardConfig;
-    let dependencies = extractDependencies(realisedTree, cardConfig);
-    let state = Object.fromEntries(
-      Object.entries(stateDefinition).map(([key, selector]) => [
-        key,
-        selector(dependencies),
-      ])
-    );
-    return { ...cardConfig, state, onRefresh, isLoading: !realisedTree };
-  });
+  return config
+    .map((cardConfig) => {
+      let { state: stateDefinition } = cardConfig;
+      let dependencies = extractDependencies(realisedTree, cardConfig);
+      let state = Object.fromEntries(
+        Object.entries(stateDefinition).map(([key, selector]) => [
+          key,
+          selector(dependencies),
+        ])
+      );
+      return { ...cardConfig, state, onRefresh, isLoading: !realisedTree };
+    })
+    .filter((cardConfig) => cardConfig.shouldRender(cardConfig.state));
 };
