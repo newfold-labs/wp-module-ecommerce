e61c994783ad362e8023fc1dde07f95d17eedbec
ashrithapradeep <46500357+ashrithapradeep@users.noreply.github.com>
2022-12-06 13:36:59 +0530

    Showing Gift and Booking Cards only when the corresponding plugins are Active (#46)
    * Modified address details sent while country with no state is selected
    
    * Update StoreAddress.js
    
    * Update StoreAddress.js
    
    * Showing Gift and Booking Cards only when the corresponding plugins are Active
    
    * Update StoreAddress.scss
    
    * Update StoreAddress.scss
    
    Co-authored-by: Ashritha <ashritha.belghatta@newfold.com>

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e64a96a1c2453107b2bf');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0b6dd263ab6019cd56d0');
