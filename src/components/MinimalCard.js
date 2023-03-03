0ec9b79ac4469327779e21b31039cefb0bc17fa8
aulisius <aulisius7@gmail.com>
2023-03-03 08:04:55 +0530

    Add task status indicator

--- src/components/MinimalCard.js
+++ src/components/MinimalCard.js
@@ -1,41 +1,71 @@
+import { Tooltip } from "@wordpress/components";
 import { useEffect, useState } from "@wordpress/element";
 import { ReactComponent as CompletedTask } from "../icons/task-completed-solid.svg";
+import { ReactComponent as InProgressTask } from "../icons/task-incomplete.svg";
 import ModalCard from "./ModalCard";
+
+/**
+ * @typedef TaskStatusIndicatorProps
+ * @property {"complete" | "inprogress" | "pending"} status
+ * @property {string?} message Used for inprogress currently
+ *
+ * @param {TaskStatusIndicatorProps} props
+ */
+function TaskStatusIndicator({ status, message }) {
+  if (status === "complete") {
+    return (
+      <div className="task-status-indicator">
+        <CompletedTask />
+      </div>
+    );
+  }
+  if (status === "inprogress") {
+    return (
+      <Tooltip text={message}>
+        <div className="task-status-indicator">
+          <InProgressTask />
+        </div>
+      </Tooltip>
+    );
+  }
+  return null;
+}
 
 export function MinimalCard(props) {
   const [showModal, setShowModal] = useState(null);
   let modal = props.modal(props.state);
   let { image: Icon } = props.assets(props.state);
-  const taskCompleted = props.state.taskCompleted;
+
+  // TODO: Deprecate taskCompleted and use taskStatus only
+  const { taskCompleted, taskStatus = "pending" } = props.state;
   useEffect(() => {
-    if (taskCompleted && showModal) {
+    if ((taskCompleted || taskStatus === "complete") && showModal) {
       setShowModal(false);
     }
-  }, [taskCompleted, showModal]);
-  let { title, actionName } = props.text(taskCompleted);
-  const buttonClickHandler = () => {
-    const data = {
-      taskCompleted: taskCompleted,
-    };
-    props.actions.buttonClick(data, setShowModal);
-  };
+  }, [taskCompleted, taskStatus, showModal]);
 
+  // TODO: We should pass complete state here.
+  let {
+    title,
+    actionName,
+    inProgressMessage = "",
+  } = props.text(taskCompleted, taskInProgress);
+  const buttonClickHandler = () => {
+    props.actions.buttonClick(props.state, setShowModal);
+  };
   return (
     <>
       <button
-        className={`nfd-ecommerce-card
-         ${taskCompleted ? "nfd-ecommerce-taskCompleted" : ""} 
-        `}
+        className={`nfd-ecommerce-card task-status-needed`}
         data-variant="minimal"
         type="button"
         onClick={buttonClickHandler}
         disabled={props.state.isDisabled}
       >
-        {taskCompleted && (
-          <div className="nfd-ecommerce-taskCompleted-image">
-            <CompletedTask />
-          </div>
-        )}
+        <TaskStatusIndicator
+          status={taskCompleted ? "complete" : taskStatus}
+          message={inProgressMessage}
+        />
         {Icon && (
           <div className="nfd-ecommerce-card-image">
             <Icon />
