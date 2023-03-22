1e0381274f22fcf1cd8b8d77271198e8ca7e6ced
Ashritha <ashritha.belghatta@newfold.com>
2023-03-22 14:34:58 +0530

    Update index.asset.php

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '463996544574ffac4dd0');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4533c630dfd412b855f6');
