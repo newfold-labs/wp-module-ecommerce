e8f1541b698faf42722e5b18234596cb61f91c6a
Ashritha <ashritha.belghatta@newfold.com>
2023-03-24 17:01:35 +0530

    Corrected styling of Store Analytics button

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '608126b443c18eaca7ea');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '96be2c3aedd9c72c968b');
