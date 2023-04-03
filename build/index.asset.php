6b514fa4a45606680f7044f48232e025dd56caf3
Ashritha <ashritha.belghatta@newfold.com>
2023-04-03 12:02:28 +0530

    fixed responsiveness in razorpay screen

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '55c8c5e0e8be8db4bdc4');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'dd4a4627cb012c5d1146');
