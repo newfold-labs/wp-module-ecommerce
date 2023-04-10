c8e8e296e4980e1c78bd1a6ce92a69d08417aa13
Roshan <85054402+roshan-singh-qa@users.noreply.github.com>
2023-04-10 12:20:44 +0530

    checking artifact download

---
+++ cypress.config.js
@@ -0,0 +1,50 @@
+const { defineConfig } = require('cypress');
+const cypressReplay = require('@replayio/cypress');
+const { phpVersion, core } = require('./.wp-env.json');
+const wpVersion = /[^/]*$/.exec(core)[0];
+
+module.exports = defineConfig({
+  // projectId: 'h78f39',
+  env: {
+    wpUsername: 'admin',
+    wpPassword: 'password',
+    wpVersion,
+    phpVersion,
+  },
+  downloadsFolder: 'tests/cypress/downloads',
+  fixturesFolder: 'tests/cypress/fixtures',
+  screenshotsFolder: 'tests/cypress/screenshots',
+  video: true,
+  videosFolder: 'tests/cypress/videos',
+  videoUploadOnPasses: false,
+  experimentalFetchPolyfill: true,
+  chromeWebSecurity: false,
+  viewportWidth: 1024,
+  viewportHeight: 768,
+  pageLoadTimeout: 120000,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Setup Replay
+      cypressReplay.default(on, config);
+
+      // Ensure that the base URL is always properly set.
+      if (config.env && config.env.baseUrl) {
+        config.baseUrl = config.env.baseUrl;
+      }
+
+      // Ensure that we have a semantically correct WordPress version number for comparisons.
+      if (config.env.wpVersion) {
+        if (config.env.wpVersion.split('.').length !== 3) {
+          config.env.wpSemverVersion = `${config.env.wpVersion}.0`;
+        } else {
+          config.env.wpSemverVersion = config.env.wpVersion;
+        }
+      }
+
+      return config;
+    },
+    baseUrl: 'http://localhost:8882',
+    specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'tests/cypress/support/index.js',
+  },
+});
--- tests/cypress/integration/example.spec.js
+++ tests/cypress/integration/example.spec.js
@@ -1,10 +1,36 @@
-describe(
-	'My First Test',
-	() => {
-		it('Does not do much!',
-			() => {
-				expect(true).to.equal(true)
-			}
-		)
-	}
-);
+const customCommandTimeout = 10000;
+var homeUrl = '/wp-admin/admin.php?page=bluehost#/home/store/general';
+
+describe('As a wp-admin user, I want to ', function () {
+  before(() => {
+    cy.activatePlugin('all');
+  });
+
+  beforeEach(() => {
+    cy.visit(homeUrl);
+    cy.injectAxe();
+    cy.contains('Tax Info', { timeout: 30000 });
+  });
+
+  it('verify "WooCommerce is not installed!" model displaying when WooCommerce plugin is not install or active', () => {
+    cy.deactivatePlugin('woocommerce');
+    cy.reload();
+    cy.contains('Uh-Oh! WooCommerce is not installed!', {
+      timeout: customCommandTimeout,
+    }).should('exist');
+    cy.contains(
+      'WooCommerce is required for this dashboard to work, install it now or contact our support team for more assistance.'
+    ).should('exist');
+    cy.contains('Install WooCommerce').should('exist');
+    cy.contains('Contact Support').should('exist');
+    const urlToVerify = 'https://www.bluehost.com/contact';
+    cy.window().then((win) => {
+      cy.stub(win, 'open')
+        .as('windowOpen')
+        .callsFake(() => {});
+    });
+    cy.contains('Contact Support').click();
+    cy.get('@windowOpen').should('be.calledWith', urlToVerify, '_blank');
+    cy.activatePlugin('woocommerce');
+  });
+});
