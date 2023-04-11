435b8570ca70e9ae691c8f668c7751c2d68ecfce
Micah Wood <micah@wpscholar.com>
2023-04-11 11:41:51 -0400

    Relocate PayPal BN code snippet to always run, even if e-commerce module isn't active. Remove check for specific endpoint and always send BN codes.
    (cherry picked from commit d17d80fe2c830dd8218f78948552faccef9f73ac)

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,7 @@ use NewfoldLabs\WP\Module\ECommerce\ECommerce;
 use function NewfoldLabs\WP\ModuleLoader\register;
 
 if ( function_exists( 'add_action' ) ) {
+
 	add_action(
 		'plugins_loaded',
 		function () {
@@ -23,6 +24,34 @@ if ( function_exists( 'add_action' ) ) {
 				)
 			);
 		}
+	);
+
+}
+
+if ( function_exists( 'add_filter' ) ) {
+
+	add_filter(
+		'http_request_args',
+		function ( $parsed_args, $url ) {
+
+			// Bail early if the request is not to PayPal's v2 checkout API
+			if ( false === stripos( wp_parse_url( $url, PHP_URL_HOST ), 'paypal.com' ) ) {
+				return $parsed_args;
+			}
+
+			// Check for an existing bn_code
+			$bn_code = isset( $parsed_args['headers']['PayPal-Partner-Attribution-Id'] ) ? $parsed_args['headers']['PayPal-Partner-Attribution-Id'] : null;
+
+			// Ensure we only set when blank, or when using one of our stale codes
+			if ( is_null( $bn_code ) || false !== stripos( $bn_code, 'yith' ) || false !== stripos( $bn_code, 'newfold' ) ) {
+				// The correct code is case-sensitive. YITH brand is uppercase, but the code is not.
+				$parsed_args['headers']['PayPal-Partner-Attribution-Id'] = 'Yith_PCP';
+			}
+
+			return $parsed_args;
+		},
+		10,
+		2
 	);
 
 }
