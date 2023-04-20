7b3739f283174a4564482babe4afe38b4e1a491b
Ashritha <ashritha.belghatta@newfold.com>
2023-04-20 17:49:39 +0530

    Display the default pages created during the onboarding flow in the Customise your store section

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b01149d783fb45e7748f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'edb4e1129120f388eea0');
