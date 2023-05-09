b385ee3384b71daa3e8bf0d64eeee806fd3ba36a
​Faizaan <aulisius@users.noreply.github.com>
2023-05-09 11:52:31 +0530

    Add Yoast UI library (#79)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '62391e444981fbd03010');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7111b3919c66337f222f');
