7d352649b34e3afd4a045f4ae0dcb0322dd30b3c
Ashritha <ashritha.belghatta@newfold.com>
2023-05-09 09:42:32 +0530

    Reading dynamically redirection url for Site Status

--- includes/ECommerce.php
+++ includes/ECommerce.php
@@ -136,7 +136,7 @@ class ECommerce {
 			$site_status_menu = array(
 				'id'     => 'site-status',
 				'parent' => 'top-secondary',
-				'href'   => admin_url( 'admin.php?page=bluehost#/home' ),
+				'href'   => admin_url('admin.php?page=' . $this->container->plugin()->id . '#/home'),
 				'title'  => '<div style="background-color: #F8F8F8; padding: 0 16px;color:#333333;">' . esc_html__( 'Site Status: ', 'wp-module-ecommerce' ) . $status . '</div>',
 				'meta'   => array(
 					'title' => esc_attr__( 'Launch Your Site', 'wp-module-ecommerce' ),
