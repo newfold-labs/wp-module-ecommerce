5ca852741b7fec2cd02add96a71cfc1acb6bafee
aulisius <aulisius7@gmail.com>
2023-05-26 00:13:42 +0530

    Add new dependency (wp-date)

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f6b045febcedb8f2f391');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'cca77caa0f892d87be3f');
