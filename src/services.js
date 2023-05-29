3d86cd8d54286d5f569e9affdb6a1e40114f9751
aulisius <aulisius7@gmail.com>
2023-05-29 16:56:07 +0530

    Remove unused code for General Settings

--- src/services.js
+++ src/services.js
@@ -2,22 +2,10 @@ import apiFetch from "@wordpress/api-fetch";
 
 export const Endpoints = {
   WP_SETTINGS: "/wp/v2/settings",
-  WC_ONBOARDING: "/wc-admin/onboarding/profile",
 };
-export async function fetchWPSettings() {
-  return apiFetch({ path: Endpoints.WP_SETTINGS });
-}
 
 export async function updateWPSettings(data) {
   return apiFetch({ path: Endpoints.WP_SETTINGS, method: "POST", data }).catch(
     (error) => {}
   );
-}
-
-export async function updateWCOnboarding(data) {
-  return apiFetch({
-    path: Endpoints.WC_ONBOARDING,
-    method: "POST",
-    data,
-  }).catch((error) => {});
 }
