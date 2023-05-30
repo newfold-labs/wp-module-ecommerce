f83011a30bc5fe03a345848f92bdc22a0d1b8dbd
Ashritha <ashritha.belghatta@newfold.com>
2023-05-30 16:54:19 -0700

    Modified shippo logo

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '0d3daed9a904675ed364');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '01963fd0b793e728543e');
