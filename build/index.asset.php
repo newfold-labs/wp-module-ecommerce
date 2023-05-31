d8f8f8e504cb6dc66638c705a92ded37900fe03f
aulisius <aulisius7@gmail.com>
2023-05-31 23:50:30 +0530

    Mark jetpack as complete if page is visited

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'bf10e9b3127b0ce981c2');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'ba62801a6a28d1530960');
