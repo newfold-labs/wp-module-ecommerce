8f72cde095cac902dc0a028606792ba82ac002bd
aulisius <aulisius7@gmail.com>
2023-06-05 19:48:24 +0530

    Hide Sales Funnel plugin

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'ba62801a6a28d1530960');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '961c12ada47d3a0eba62');
