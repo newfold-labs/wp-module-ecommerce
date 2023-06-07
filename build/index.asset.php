9131a95c480a55a577f2afcb3a62a898645667b7
Ashritha <ashritha.belghatta@newfold.com>
2023-06-06 21:49:37 -0700

    Updated the version to v1.0.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '961c12ada47d3a0eba62');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '374cd6d6c0aeaa4e4bc6');
