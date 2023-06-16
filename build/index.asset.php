23bb2210c9277f9378b1aaa22ea261aea5b538c8
ashritha <ashrithabelghatta@gmail.com>
2023-06-16 19:57:03 +0530

    Referencing site as store only when the site have ecommerce capability

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'd5b72d8d5b4f4639d194');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '783229073cbf147fd522');
