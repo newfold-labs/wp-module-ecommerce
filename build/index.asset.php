742a4f3b1742a0ef3adf1d2607ad660148d5e772
aulisius <aulisius7@gmail.com>
2023-06-22 17:52:56 +0530

    Bump version to 1.0.6

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'ae684ef8a13bd749f22f');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'dd84910f590cf87807e9');
