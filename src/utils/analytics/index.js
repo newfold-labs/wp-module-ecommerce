49ccf997077c63ac478eacd67108a72113628351
sumathi3399 <sumathivarma3399@gmail.com>
2023-07-03 15:49:41 +0530

    PRESS4-267-added hiveanalytics

--- src/utils/analytics/index.js
+++ src/utils/analytics/index.js
@@ -1,9 +1,17 @@
-import { HiiveAnalytics, HiiveEvent } from '@newfold-labs/js-utility-ui-analytics';
+import {
+  HiiveAnalytics,
+  HiiveEvent,
+} from "@newfold-labs/js-utility-ui-analytics";
 
-export const trackHiiveEvent = ( action, value ) => {
-	const hiiveEvent = new HiiveEvent( 'wp-module-ecommerce', action, {
-		value,
-		timestamp: Date.now(),
-	}, 'wp-module-ecommerce' );
-    HiiveAnalytics.send( hiiveEvent );
+export const trackHiiveEvent = (action, value) => {
+  const hiiveEvent = new HiiveEvent(
+    "wp-module-ecommerce",
+    action,
+    {
+      value,
+      timestamp: Date.now(),
+    },
+    "wp-module-ecommerce"
+  );
+  HiiveAnalytics.send(hiiveEvent);
 };
