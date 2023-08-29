55bf40ffa75e66ac98a79b34616ac41060f9c7ed
sumathi3399 <sumathivarma3399@gmail.com>
2023-08-29 18:08:24 +0530

    PRESS4-303 | added url to payments tab in homepage

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'bc2c6eedee95b95d9178');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '199588374255e28652cb');
