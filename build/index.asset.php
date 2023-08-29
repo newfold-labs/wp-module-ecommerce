a8eabc3a10e69bc21bc43af7484207f4cd273d90
Evan Mullins <evanrm@gmail.com>
2023-08-29 16:48:08 -0400

    version bump to 1.2.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd92107dfb0bd29fc36c3');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6452908daa8db27cd7f1');
