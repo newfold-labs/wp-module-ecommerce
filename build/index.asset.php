9f95e0066e4d169871092f75afe76966378cd3f5
sumathi3399 <sumathivarma3399@gmail.com>
2023-08-29 13:57:23 +0530

    PRESS4-301 | addressed QA changes for learn more

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '854f3fd0d5605193ac3a');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a234749b4c67c0ede411');
