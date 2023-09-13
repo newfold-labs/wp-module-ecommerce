87ac40d59b15a1e6d8607c33ebb2697b48cdbffe
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-09-13 12:11:03 +0530

    shipping options removed && isWoo key added to newfoldruntime file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd92107dfb0bd29fc36c3');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8e53e1c28308c2476fa8');
