3fe8b50d6f5b6f770d007502929355c08d33a0a3
Arati Bhandare <arati.bhandare@newfold.com>
2023-09-14 15:47:08 +0530

    Brazalian translations added for missing texts

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0aa4055ae6a647b93098');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '09869c0ad3a1903c8523');
