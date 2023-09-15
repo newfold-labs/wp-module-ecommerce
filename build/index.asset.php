ac677a6b751e9da197f32b9f52622ab04f42707e
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-09-15 15:13:03 +0530

    version bump v1.2.4

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8e53e1c28308c2476fa8');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'ad13a34379c1064004a7');
