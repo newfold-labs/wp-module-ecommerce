b4fbee80be6456d8efe1a085f8d0e6584fae90df
sumathi3399 <sumathivarma3399@gmail.com>
2023-09-20 02:03:48 +0530

    PRESS4-341 | CHANGES FOR WC

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0628557369ee205f19b3');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0b126e868b04b9f8603e');
