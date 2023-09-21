e13dd4c5372ace8846cdb2d8e12df710505da0fb
sumathi3399 <sumathivarma3399@gmail.com>
2023-09-21 13:08:43 +0530

    PRESS4-304 | updated the button styles

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a0cedbe231e0c2480ecf');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'df0c56c302faf68cbb3d');
--- includes/Partials/CaptiveFlow.php
+++ includes/Partials/CaptiveFlow.php
@@ -10,10 +10,7 @@ class CaptiveFlow {
 	static $RAZORPAY_CAPTIVE_FLOW = 'nfd-ecommerce-captive-flow-razorpay';
 
 	public static function init() {
-		add_action( 'admin_menu', array( __CLASS__, 'register_page' ) );
 		add_action( 'rest_api_init', array( __CLASS__, 'register_options' ) );
-		// add_action( 'load-admin_page_' . self::$PAYPAL_CAPTIVE_FLOW, array( __CLASS__, 'enqueue_styles' ), 100 );
-		// add_action( 'load-admin_page_' . self::$SHIPPO_CAPTIVE_FLOW, array( __CLASS__, 'enqueue_styles' ), 100 );
 	}
 
 	public static function register_options() {
@@ -70,41 +67,5 @@ class CaptiveFlow {
 		);
 		\wp_enqueue_style( 'nfd-ecommerce-captive', NFD_ECOMMERCE_PLUGIN_URL . 'vendor/newfold-labs/wp-module-ecommerce/includes/Partials/template.css', null, '1', 'screen' );
 	}
-
-	public static function register_page() {
-		// \add_submenu_page(
-		// 	null,
-		// 	null,
-		// 	null,
-		// 	Permissions::ADMIN,
-		// 	self::$PAYPAL_CAPTIVE_FLOW,
-		// 	array( __CLASS__, 'render_paypal' ),
-		// 	100
-		// );
-		// \add_submenu_page(
-		// 	null,
-		// 	null,
-		// 	null,
-		// 	Permissions::ADMIN,
-		// 	self::$SHIPPO_CAPTIVE_FLOW,
-		// 	array( __CLASS__, 'render_shippo' ),
-		// 	100
-		// );
-	}
-
-	// public static function render_paypal() {
-	// 	echo PHP_EOL;
-	// 	echo '<div id="nfd-ecommerce" class="nfd-ecommerce-captive-flow">';
-	// 	echo do_action( self::$PAYPAL_CAPTIVE_FLOW );
-	// 	echo '</div>';
-	// 	echo PHP_EOL;
-	// }
-
-	// public static function render_shippo() {
-	// 	echo PHP_EOL;
-	// 	echo '<div id="nfd-ecommerce" class="nfd-ecommerce-captive-flow">';
-	// 	echo do_action( self::$SHIPPO_CAPTIVE_FLOW );
-	// 	echo '</div>';
-	// 	echo PHP_EOL;
-	// }
+	
 }
