53da5d419ba1bb8d21d5ae812dd713db0aaee63d
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-10-03 18:37:49 +0530

    bluehost signup commented

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '34269e10651522fd8b76');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e1c66530ff4073a2ede6');
