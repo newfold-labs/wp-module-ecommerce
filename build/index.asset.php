42806018a7183a3fb84bf87ee29d29f2f3ca2c80
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-10-16 15:39:58 +0530

    link to add ons

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6ded608fd9282ac24dc2');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'dfffcf7146f9a8f15f28');
