bd0004de9165b76612a2853339fbe85a49d73485
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-11-06 13:46:46 +0530

    removed temporary code && auto reload of woocommerce installed && version bump to 1.3.9

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e076e1d03f91ef19a291');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b411e7fa1e5e99ae3542');
