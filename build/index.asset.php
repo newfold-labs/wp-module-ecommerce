2130081c7328d5b14cd47f892cecce2311e32c52
Ramya krishna <ramyakrishnaimminni@gmail.com>
2023-11-06 12:48:50 +0530

    Ecommerce version changed to `1.3.8`

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b3f83f29834463a274f5');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e076e1d03f91ef19a291');
