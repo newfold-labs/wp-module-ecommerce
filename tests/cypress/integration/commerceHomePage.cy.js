58abc22d6dbbc83af04bddf7f22e56afe308927e
sangeetha-nayak <sangeetha.nayak@newfold.com>
2023-11-10 17:34:06 +0530

    changes as per comments

--- tests/cypress/integration/commerceHomePage.cy.js
+++ tests/cypress/integration/commerceHomePage.cy.js
@@ -1,4 +1,5 @@
 import 'cypress-iframe'
+import { GetPluginId } from './wp-module-support/pluginID.cy';
 
 const customCommandTimeout = 40000;
 
@@ -6,21 +7,21 @@ const customCommandTimeout = 40000;
 describe(
 	'Bluehost WP Admin Home Page',
 	() => {
-		before(()=>{
+		before(() => {
 			cy.activatePlugin('all');
 		})
 
-		beforeEach(()=> {
-			cy.visit('/wp-admin/admin.php?page=bluehost#/home')
+		beforeEach(() => {
+			cy.visit('/wp-admin/admin.php?page=' + GetPluginId + '#/home')
 		})
 
-		it('Test Next steps for your site exists', ()=> {
-			cy.contains('Next steps for your site', {timeout: customCommandTimeout}).should('exist')
+		it('Test Next steps for your site exists', () => {
+			cy.contains('Next steps for your site', { timeout: customCommandTimeout }).should('exist')
 		})
 
-		it('Test Ready to go next level ', ()=> {
-			cy.contains('Ready to go to the next level?', {timeout: customCommandTimeout}).should('exist')
+		it('Test Ready to go next level ', () => {
+			cy.contains('Ready to go to the next level?', { timeout: customCommandTimeout }).should('exist')
 		})
-	
+
 	}
 );
