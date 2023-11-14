7638965f3e8d5e9977dda7f4000695ce9d7bd3bb
manikantakailasa <manikanta.kailasa@gmail.com>
2023-11-14 15:24:27 +0530

    ui colour and border radius fix

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '4697b2445a35ed2572dc');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'd2e3fe4259ee55439319');
