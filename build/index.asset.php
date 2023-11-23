a8c0981471afec7ad8dc123b86d0c92bc2795221
Arati Bhandare <arati.bhandare@newfold.com>
2023-11-23 16:57:07 +0530

    Facebook Connect Module related changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '66938dc93e0a18261fc8');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '4903fbfc71f7fbcf3a3f');
--- includes/RestApi/IntegrationsController.php
+++ includes/RestApi/IntegrationsController.php
@@ -157,7 +157,7 @@ class IntegrationsController {
 			if ( ! $hiive_token ) {}
 		return new \WP_REST_Response(
 			array(
-				'token' => $hiive_token ? $hiive_token : 'test',
+				'token' => $hiive_token ? $hiive_token : 'test2',
 			),
 			200
 		);
