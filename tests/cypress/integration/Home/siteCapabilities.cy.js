60e753b8bf03d932d512404f136a2366bfb9c320
sangeetha-nayak <sangeetha.nayak@newfold.com>
2023-11-29 14:04:35 +0530

    try just delete

--- tests/cypress/integration/Home/siteCapabilities.cy.js
+++ tests/cypress/integration/Home/siteCapabilities.cy.js
@@ -22,19 +22,24 @@ describe(
         })
 
         it('Default test', () => {
-            // cy.exec(`npx wp-env run cli wp option update _transient_nfd_site_capabilities '${ecomTrue}'`).then((result) => {
+
+            cy.exec( 'npx wp-env run cli wp option delete _transient_nfd_site_capabilities' )
+            // cy.exec(`wp-env run cli wp option set _transient_nfd_site_capabilities '${ecomTrue}'`).then((result) => {
             //     cy.log(result.stdout);
             //     cy.log(result.stderr);
             // })
 
             // cy.exec(`wp-env run cli wp option update _transient_nfd_site_capabilities '${ecomTrue}' --path c/Users/sangeetha.n1/Local Sites/bhlatest/app/public`, { timeout: 20000 }).then((result) => {
                 // cy.exec(`npx wp-env run cli wp option set mm_brand Bluehost`, { timeout: 20000 }).then((result) => {
-            cy.exec(`npx wp-env run cli wp option update _transient_nfd_site_capabilities 'a:0:{}'`, { timeout: 20000 }).then((result) => {
-                cy.log(result.stdout);
-                cy.log(result.stderr);
-            })
-            cy.get(`.${getAppId()}-app-navitem-Store`).click()
-            cy.contains('Sales and Discounts').should('exist')
+            // cy.exec(`npx wp-env run cli wp option update _transient_nfd_site_capabilities '${ecomTrue}'`, { timeout: 20000, log: true }).then((result) => {
+            //     cy.log(result.stdout);
+            //     cy.log(result.stderr);
+            // })
+
+            // cy.exec('npx wp-env run cli wp option update _transient_nfd_site_capabilities \'a:7:{s:11:"canAccessAI";b:1;s:18:"canAccessGlobalCTB";b:0;s:19:"canAccessHelpCenter";b:1;s:11:"hasEcomdash";b:0;s:15:"hasYithExtended";b:0;s:11:"isEcommerce";b:1;s:8:"isJarvis";b:1;}\'', {log: true})
+
+            // cy.get(`.${getAppId()}-app-navitem-Store`).click()
+            // cy.contains('Sales and Discounts').should('exist')
             
         })
         
