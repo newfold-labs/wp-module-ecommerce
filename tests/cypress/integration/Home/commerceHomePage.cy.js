98cdac89501afd51d5f7477d616a94d30619022b
sangeetha-nayak <sangeetha.nayak@newfold.com>
2023-12-04 18:32:47 +0530

    Home page tests- Live mode

--- tests/cypress/integration/Home/commerceHomePage.cy.js
+++ tests/cypress/integration/Home/commerceHomePage.cy.js
@@ -1,10 +1,11 @@
+import { forEach } from 'lodash';
 import { GetPluginId } from '../wp-module-support/pluginID.cy';
 import { comingSoon } from '../wp-module-support/utils.cy';
 
 const customCommandTimeout = 60000;
 
 
-describe(
+describe.skip(
 	'Commerce Home Page- Coming soon mode',
 	() => {
 		before(() => {
@@ -52,5 +53,47 @@ describe(
 			cy.contains('Ready to go to the next level?', { timeout: customCommandTimeout }).should('exist')
 		})
 
+	},
+);
+
+describe(
+	'Commerce Home Page- Live mode',
+	() => {
+		before(() => {
+			cy.exec(`npx wp-env run cli wp plugin deactivate woocommerce`, {
+				failOnNonZeroExit: false,
+			});
+			cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/home')
+			comingSoon(false)
+		})
+
+		beforeEach(() => {
+			cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/home')
+		})
+
+		it('Verify presense of Ready to go to next level? canvas', () => {
+			cy.get('.nfd-flex.nfd-gap-4', { timeout: customCommandTimeout }).eq(2).as('readyToGoNextLevel').should('exist')
+			cy.get('@readyToGoNextLevel', { timeout: customCommandTimeout }).find('h1').should('have.text', 'Ready to go to the next level?')
+			cy.get('@readyToGoNextLevel').find('div span').should('exist').and('have.text', 'Your site is live to the world!')
+		})
+
+		it('Verify by default View Site option should be displayed', () => {
+			cy.contains('.nfd-button--primary', 'View Site', {timeout: customCommandTimeout}).should('exist').invoke('removeAttr', 'target').click()
+			cy.url().should('eq', Cypress.config().baseUrl + '/')
+		})
+
+		it('Verify Next steps for your site section', () => {
+			const steps = ['Add a new page to your site', 'Upload media to your site', 'Enable Jetpack to connect to your social media accounts']
+			cy.get('.nfd-grid.nfd-gap-4', { timeout: customCommandTimeout }).as('nextSteps').should('exist')
+			cy.get('@nextSteps').find('h1').should('have.text', 'Next steps for your site')
+			cy.get('@nextSteps').find('p').should('have.text', 'You\'re just a few steps away from sharing your store with the world!')
+			cy.get('@nextSteps').find('ul li').each((item, index, list) => {
+				// Returns the elements from the cy.get command
+				expect(list).to.have.length(3);
+			
+				// Returns the current element from the loop
+				expect(Cypress.$(item).text()).to.eq(steps[index]);
+			});
+		})
 	},
 );
