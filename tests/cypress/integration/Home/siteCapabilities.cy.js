bebdd95cdeb0ef17c7c331262f9e002331b990b9
sangeetha-nayak <sangeetha.nayak@newfold.com>
2023-12-05 15:43:19 +0530

    try delete and set in all specs

--- tests/cypress/integration/Home/siteCapabilities.cy.js
+++ tests/cypress/integration/Home/siteCapabilities.cy.js
@@ -12,27 +12,28 @@ describe(
         const cTBFalseYithTrue = 'a:7:{s:11:"canAccessAI";b:1;s:18:"canAccessGlobalCTB";b:0;s:19:"canAccessHelpCenter";b:1;s:11:"hasEcomdash";b:0;s:15:"hasYithExtended";b:1;s:11:"isEcommerce";b:1;s:8:"isJarvis";b:1;}'
         const cTBTrueYithFalse = 'a:7:{s:11:"canAccessAI";b:1;s:18:"canAccessGlobalCTB";b:1;s:19:"canAccessHelpCenter";b:1;s:11:"hasEcomdash";b:0;s:15:"hasYithExtended";b:0;s:11:"isEcommerce";b:1;s:8:"isJarvis";b:1;}'
 
-        before(() => {
-            cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/store')
-            cy.get('.nfd-app-section-content .nfd-button--upsell').eq(0).invoke('text').then((btnText) => {
-                if (btnText == 'Install WooCommerce') {
-                    cy.get('.nfd-app-section-content .nfd-button--upsell').contains(btnText).click();
-                    cy.get('.nfd-notification--success', { timeout: customCommandTimeout }).contains('WooCommerce has been installed successfully').should('exist')
-                }
-            })
-        })
+        // before(() => {
+        //     cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/store')
+        //     cy.get('.nfd-app-section-content .nfd-button--upsell').eq(0).invoke('text').then((btnText) => {
+        //         if (btnText == 'Install WooCommerce') {
+        //             cy.get('.nfd-app-section-content .nfd-button--upsell').contains(btnText).click();
+        //             cy.get('.nfd-notification--success', { timeout: customCommandTimeout }).contains('WooCommerce has been installed successfully').should('exist')
+        //         }
+        //     })
+        // })
 
         beforeEach(() => {
             cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/home')
         })
+
         it('Verify Install Now is shown when canAccessGlobalCTB and hasYithExtended set to true', () => {
             // cy.exec( `npx wp-env run cli wp option get _transient_nfd_site_capabilities` )
-            cy.exec( `npx wp-env run cli wp option delete mm_brand`, {failOnNonZeroExit: false})
-            cy.exec(`npx wp-env run cli wp option set mm_brand bluehost`, { timeout: customCommandTimeout }).then((result) => {
+            cy.exec( `npx wp-env run cli wp option delete _transient_nfd_site_capabilities`, {failOnNonZeroExit: false})
+            cy.exec(`npx wp-env run cli wp option set _transient_nfd_site_capabilities '${cTBAndYithTrue}'`, { timeout: customCommandTimeout }).then((result) => {
                 cy.log(result.stdout);
                 cy.log(result.stderr);
             })
-            cy.reload();
+            // cy.reload();
             // cy.get(`.${appId}-app-navitem-Store`, {timeout: customCommandTimeout}).click()
             // cy.get(`.${appId}-app-subnavitem`, {timeout: customCommandTimeout}).contains('Sales & Discounts').as('salesTab').should('exist')
             // cy.get('@salesTab').click();
@@ -40,30 +41,30 @@ describe(
             
         })
 
-        it.skip('Verify Install Now is shown when canAccessGlobalCTB is false and hasYithExtended set to true', () => {
-            // cy.exec( `npx wp-env run cli wp option delete _transient_nfd_site_capabilities`, { timeout: customCommandTimeout } )
-            cy.exec(`npx wp-env run cli wp option update _transient_nfd_site_capabilities '${cTBFalseYithTrue}'`, { timeout: customCommandTimeout }).then((result) => {
+        it('Verify Install Now is shown when canAccessGlobalCTB is false and hasYithExtended set to true', () => {
+            cy.exec( `npx wp-env run cli wp option delete _transient_nfd_site_capabilities`, { timeout: customCommandTimeout } )
+            cy.exec(`npx wp-env run cli wp option set _transient_nfd_site_capabilities '${cTBFalseYithTrue}'`, { timeout: customCommandTimeout }).then((result) => {
                 cy.log(result.stdout);
                 cy.log(result.stderr);
             })
-            cy.reload();
-            cy.get(`.${appId}-app-navitem-Store`).click()
-            cy.get(`.${appId}-app-subnavitem`).contains('Sales & Discounts').as('salesTab').should('exist')
-            cy.get('@salesTab').click();
-            cy.get('.nfd-button--upsell').contains('Install now').should('exist')
+            // cy.reload();
+            // cy.get(`.${appId}-app-navitem-Store`).click()
+            // cy.get(`.${appId}-app-subnavitem`).contains('Sales & Discounts').as('salesTab').should('exist')
+            // cy.get('@salesTab').click();
+            // cy.get('.nfd-button--upsell').contains('Install now').should('exist')
         })
 
-        it.skip('Verify Install Now is shown when canAccessGlobalCTB is true and hasYithExtended set to false', () => {
-            // cy.exec( `npx wp-env run cli wp option delete _transient_nfd_site_capabilities` , { timeout: customCommandTimeout })
-            // cy.exec(`npx wp-env run cli wp option update _transient_nfd_site_capabilities '${cTBTrueYithFalse}'`, { timeout: customCommandTimeout }).then((result) => {
-            //     cy.log(result.stdout);
-            //     cy.log(result.stderr);
-            // })
-            cy.reload();
-            cy.get(`.${appId}-app-navitem-Store`).click()
-            cy.get(`.${appId}-app-subnavitem`).contains('Sales & Discounts').as('salesTab').should('exist')
-            cy.get('@salesTab').click();
-            cy.get('.nfd-button--upsell').contains('Buy now').should('exist')
+        it('Verify Buy Now is shown when canAccessGlobalCTB is true and hasYithExtended set to false', () => {
+            cy.exec( `npx wp-env run cli wp option delete _transient_nfd_site_capabilities` , { timeout: customCommandTimeout })
+            cy.exec(`npx wp-env run cli wp option set _transient_nfd_site_capabilities '${cTBTrueYithFalse}'`, { timeout: customCommandTimeout }).then((result) => {
+                cy.log(result.stdout);
+                cy.log(result.stderr);
+            })
+            // cy.reload();
+            // cy.get(`.${appId}-app-navitem-Store`).click()
+            // cy.get(`.${appId}-app-subnavitem`).contains('Sales & Discounts').as('salesTab').should('exist')
+            // cy.get('@salesTab').click();
+            // cy.get('.nfd-button--upsell').contains('Buy now').should('exist')
         })
         
     });
