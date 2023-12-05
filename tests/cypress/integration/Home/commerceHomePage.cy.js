9812cb0b4cc7186a3f7c37525872f07ce67379ce
sangeetha-nayak <sangeetha.nayak@newfold.com>
2023-12-05 11:22:08 +0530

    Home Page tests next steps

--- tests/cypress/integration/Home/commerceHomePage.cy.js
+++ tests/cypress/integration/Home/commerceHomePage.cy.js
@@ -4,96 +4,221 @@ import { comingSoon } from '../wp-module-support/utils.cy';
 
 const customCommandTimeout = 60000;
 
+describe( 'Commerce Home Page- Coming soon mode', () => {
+	before( () => {
+		cy.exec( `npx wp-env run cli wp plugin deactivate woocommerce`, {
+			failOnNonZeroExit: false,
+		} );
+		cy.visit( '/wp-admin/admin.php?page=' + GetPluginId() + '#/home' );
+		comingSoon( true );
+	} );
 
-describe.skip(
-	'Commerce Home Page- Coming soon mode',
-	() => {
-		before(() => {
-			cy.exec(`npx wp-env run cli wp plugin deactivate woocommerce`, {
-				failOnNonZeroExit: false,
-			});
-			cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/home')
-			comingSoon(true)
-		})
+	beforeEach( () => {
+		cy.visit( '/wp-admin/admin.php?page=' + GetPluginId() + '#/home' );
+	} );
 
-		beforeEach(() => {
-			cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/home')
-		})
+	it( 'Verify Congrats on your new site message, coming soon alert', () => {
+		cy.contains( 'Congrats on your new site!', {
+			timeout: customCommandTimeout,
+		} ).should( 'exist' );
+		cy.get( '.nfd-alert.nfd-alert--warning', {
+			timeout: customCommandTimeout,
+		} )
+			.as( 'comingsoonalert' )
+			.should( 'exist' );
+		cy.get( '@comingsoonalert' )
+			.find( '.nfd-validation-icon' )
+			.should( 'exist' );
+		cy.get( '@comingsoonalert' )
+			.find( '.nfd-validation-message' )
+			.should( 'exist' )
+			.and(
+				'have.text',
+				'Your site is currently displaying a "Coming Soon" page.'
+			);
+	} );
 
-		it('Verify Congrats on your new site message, coming soon alert', () => {
-			cy.contains('Congrats on your new site!', { timeout: customCommandTimeout }).should('exist')
-			cy.get('.nfd-alert.nfd-alert--warning', { timeout: customCommandTimeout }).as('comingsoonalert').should('exist')
-			cy.get('@comingsoonalert').find('.nfd-validation-icon').should('exist')
-			cy.get('@comingsoonalert').find('.nfd-validation-message').should('exist').and('have.text', 'Your site is currently displaying a "Coming Soon" page.')
+	it( 'Verify Site Preview flex and View your site option', () => {
+		cy.get( '.nfd-justify-start > .nfd-flex-col > .nfd-absolute', {
+			timeout: customCommandTimeout,
+		} )
+			.as( 'sitePreviewFlex' )
+			.should( 'exist' );
+		cy.get( '@sitePreviewFlex' )
+			.trigger( 'mouseover' )
+			.find( 'a.nfd-button' )
+			.should( 'have.text', 'View your site' )
+			.invoke( 'removeAttr', 'target' )
+			.click();
+		cy.url().should( 'eq', Cypress.config().baseUrl + '/' );
+		cy.go( 'back' );
+	} );
 
-		})
+	it( 'Verify presense of Ready to go to live? canvas', () => {
+		cy.get( '.nfd-px-4', { timeout: customCommandTimeout } )
+			.as( 'readyToGoNextLevel' )
+			.should( 'exist' );
+		cy.get( '@readyToGoNextLevel', { timeout: customCommandTimeout } )
+			.find( '.nfd-flex-1 h1' )
+			.should( 'have.text', 'Ready to go live?' );
+		cy.get( '@readyToGoNextLevel' )
+			.find( '.nfd-flex-1 span' )
+			.should( 'exist' )
+			.and(
+				'have.text',
+				"Preview your Site before setting it live to make sure everything is how you want it.\nOnce you're ready, set your site live!"
+			);
+		cy.get( '@readyToGoNextLevel', { timeout: customCommandTimeout } )
+			.find( '.nfd-flex-none > .nfd-button--secondary' )
+			.should( 'exist' )
+			.and( 'have.text', 'View your site' );
+		cy.get( '@readyToGoNextLevel' )
+			.find( '.nfd-flex-none .nfd-button--upsell' )
+			.should( 'exist' )
+			.and( 'have.text', 'Launch your site' );
+	} );
 
-		it('Verify Site Preview flex and View your site option', () => {
-			cy.get('.nfd-justify-start > .nfd-flex-col > .nfd-absolute', { timeout: customCommandTimeout }).as('sitePreviewFlex').should('exist')
-			cy.get('@sitePreviewFlex').trigger('mouseover').find('a.nfd-button').should('have.text', 'View your site').invoke('removeAttr', 'target').click()
-			cy.url().should('eq', Cypress.config().baseUrl + '/')
-			cy.go('back')
-		})
+	it( 'Verify Visit your site and Launch your site functionality', () => {
+		cy.get( '.nfd-flex-none > .nfd-button--secondary', {
+			timeout: customCommandTimeout,
+		} )
+			.invoke( 'removeAttr', 'target' )
+			.click();
+		cy.url().should( 'eq', Cypress.config().baseUrl + '/' );
+		cy.go( 'back' );
+		cy.get( '.nfd-flex-none > .nfd-button--upsell' ).click();
+		cy.get( '.nfd-notification--success' )
+			.should( 'exist' )
+			.and( 'contain.text', 'Your site is online now!' );
+		cy.contains( 'Ready to go to the next level?', {
+			timeout: customCommandTimeout,
+		} ).should( 'exist' );
+	} );
+} );
 
-		it('Verify presense of Ready to go to live? canvas', () => {
-			cy.get('.nfd-px-4', { timeout: customCommandTimeout }).as('readyToGoNextLevel').should('exist')
-			cy.get('@readyToGoNextLevel', { timeout: customCommandTimeout }).find('.nfd-flex-1 h1').should('have.text', 'Ready to go live?')
-			cy.get('@readyToGoNextLevel').find('.nfd-flex-1 span').should('exist').and('have.text', 'Preview your Site before setting it live to make sure everything is how you want it.\nOnce you\'re ready, set your site live!')
-			cy.get('@readyToGoNextLevel', { timeout: customCommandTimeout }).find('.nfd-flex-none > .nfd-button--secondary').should('exist').and('have.text', 'View your site')
-			cy.get('@readyToGoNextLevel').find('.nfd-flex-none .nfd-button--upsell').should('exist').and('have.text', 'Launch your site')
+describe( 'Commerce Home Page- Live mode', () => {
+	before( () => {
+		cy.exec( `npx wp-env run cli wp plugin deactivate woocommerce`, {
+			failOnNonZeroExit: false,
+		} );
+		cy.visit( '/wp-admin/admin.php?page=' + GetPluginId() + '#/home' );
+		comingSoon( false );
+	} );
 
-		})
+	beforeEach( () => {
+		cy.visit( '/wp-admin/admin.php?page=' + GetPluginId() + '#/home' );
+	} );
 
-		it('Verify Visit your site and Launch your site functionality', () => {
-			cy.get('.nfd-flex-none > .nfd-button--secondary', { timeout: customCommandTimeout }).invoke('removeAttr', 'target').click()
-			cy.url().should('eq', Cypress.config().baseUrl + '/')
-			cy.go('back')
-			cy.get('.nfd-flex-none > .nfd-button--upsell').click()
-			cy.get('.nfd-notification--success').should('exist').and('contain.text', 'Your site is online now!')
-			cy.contains('Ready to go to the next level?', { timeout: customCommandTimeout }).should('exist')
-		})
+	it( 'Verify presense of Ready to go to next level? canvas', () => {
+		cy.get( '.nfd-flex.nfd-gap-4', { timeout: customCommandTimeout } )
+			.eq( 2 )
+			.as( 'readyToGoNextLevel' )
+			.should( 'exist' );
+		cy.get( '@readyToGoNextLevel', { timeout: customCommandTimeout } )
+			.find( 'h1' )
+			.should( 'have.text', 'Ready to go to the next level?' );
+		cy.get( '@readyToGoNextLevel' )
+			.find( 'div span' )
+			.should( 'exist' )
+			.and( 'have.text', 'Your site is live to the world!' );
+	} );
 
-	},
-);
+	it( 'Verify by default View Site option should be displayed', () => {
+		cy.contains( '.nfd-button--primary', 'View Site', {
+			timeout: customCommandTimeout,
+		} )
+			.should( 'exist' )
+			.invoke( 'removeAttr', 'target' )
+			.click();
+		cy.url().should( 'eq', Cypress.config().baseUrl + '/' );
+	} );
 
-describe(
-	'Commerce Home Page- Live mode',
-	() => {
-		before(() => {
-			cy.exec(`npx wp-env run cli wp plugin deactivate woocommerce`, {
-				failOnNonZeroExit: false,
-			});
-			cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/home')
-			comingSoon(false)
-		})
-
-		beforeEach(() => {
-			cy.visit('/wp-admin/admin.php?page=' + GetPluginId() + '#/home')
-		})
-
-		it('Verify presense of Ready to go to next level? canvas', () => {
-			cy.get('.nfd-flex.nfd-gap-4', { timeout: customCommandTimeout }).eq(2).as('readyToGoNextLevel').should('exist')
-			cy.get('@readyToGoNextLevel', { timeout: customCommandTimeout }).find('h1').should('have.text', 'Ready to go to the next level?')
-			cy.get('@readyToGoNextLevel').find('div span').should('exist').and('have.text', 'Your site is live to the world!')
-		})
-
-		it('Verify by default View Site option should be displayed', () => {
-			cy.contains('.nfd-button--primary', 'View Site', {timeout: customCommandTimeout}).should('exist').invoke('removeAttr', 'target').click()
-			cy.url().should('eq', Cypress.config().baseUrl + '/')
-		})
-
-		it('Verify Next steps for your site section', () => {
-			const steps = ['Add a new page to your site', 'Upload media to your site', 'Enable Jetpack to connect to your social media accounts']
-			cy.get('.nfd-grid.nfd-gap-4', { timeout: customCommandTimeout }).as('nextSteps').should('exist')
-			cy.get('@nextSteps').find('h1').should('have.text', 'Next steps for your site')
-			cy.get('@nextSteps').find('p').should('have.text', 'You\'re just a few steps away from sharing your store with the world!')
-			cy.get('@nextSteps').find('ul li').each((item, index, list) => {
+	it( 'Verify Next steps for your site section', () => {
+		const steps = [
+			'Add a new page to your site',
+			'Upload media to your site',
+			'Enable Jetpack to connect to your social media accounts',
+		];
+		cy.get( '.nfd-grid.nfd-gap-4', { timeout: customCommandTimeout } )
+			.as( 'nextSteps' )
+			.should( 'exist' );
+		cy.get( '@nextSteps' )
+			.find( 'h1' )
+			.should( 'have.text', 'Next steps for your site' );
+		cy.get( '@nextSteps' )
+			.find( 'p' )
+			.should(
+				'have.text',
+				"You're just a few steps away from sharing your store with the world!"
+			);
+		cy.get( '@nextSteps' )
+			.find( 'ul li' )
+			.each( ( item, index, list ) => {
 				// Returns the elements from the cy.get command
-				expect(list).to.have.length(3);
-			
+				expect( list ).to.have.length( 3 );
+
 				// Returns the current element from the loop
-				expect(Cypress.$(item).text()).to.eq(steps[index]);
-			});
-		})
-	},
-);
+				expect( Cypress.$( item ).text() ).to.eq( steps[ index ] );
+			} );
+	} );
+
+	it( 'Verify Next steps when experience level is novice', () => {
+		const steps = [
+			'Sign up for Bluehost WordPress Academy',
+			'Sign up for Yoast SEO Academy',
+			'Add a new page to your site',
+			'Upload media to your site',
+			'Enable Jetpack to connect to your social media accounts',
+		];
+
+		const other_steps = [
+			'Sign up for Yoast SEO Academy',
+			'Add a new page to your site',
+			'Upload media to your site',
+			'Enable Jetpack to connect to your social media accounts',
+		];
+
+		cy.exec(
+			`npx wp-env run cli wp option set onboarding_experience_level 1`
+		);
+		cy.wait(2000)
+		cy.get( '.nfd-grid.nfd-gap-4', { timeout: customCommandTimeout } )
+			.as( 'nextSteps' )
+			.should( 'exist' );
+
+		cy.get( '@nextSteps' )
+			.find( 'ul li' )
+			.each( ( item, index, list ) => {
+				if ( GetPluginId() == 'bluehost' ) {
+					// Returns the elements from the cy.get command
+					expect( list ).to.have.length( 5 );
+					// Returns the current element from the loop
+					expect( Cypress.$( item ).text() ).to.eq( steps[ index ] );
+				} else {
+					expect( list ).to.have.length( 4 );
+					// Returns the current element from the loop
+					expect( Cypress.$( item ).text() ).to.eq(
+						other_steps[ index ]
+					);
+				}
+			} );
+	} );
+
+	it( 'Verify Add a new page to your site step', () => {
+		cy.contains(
+			'.nfd-grid.nfd-gap-4 ul li a',
+			'Add a new page to your site',
+			{ timeout: customCommandTimeout }
+		)
+			.as( 'addPage' )
+			.click();
+		cy.url().should(
+			'eq',
+			Cypress.config().baseUrl +
+				'/wp-admin/post-new.php?post_type=page&return_to_nfd'
+		);
+		cy.go( 'back' );
+		cy.wait( 2000 );
+		cy.get( '@addPage' ).should( 'not.exist' );
+	} );
+} );
