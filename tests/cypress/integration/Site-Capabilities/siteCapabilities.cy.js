98fce6ef0f9e8107039b24fa78c8bc1bcc5f55db
sangeetha-nayak <sangeetha.nayak@newfold.com>
2023-12-29 13:11:07 +0530

    small changes to exec statement

--- tests/cypress/integration/Site-Capabilities/siteCapabilities.cy.js
+++ tests/cypress/integration/Site-Capabilities/siteCapabilities.cy.js
@@ -2,24 +2,24 @@ import { GetPluginId, getAppId } from '../wp-module-support/pluginID.cy';
 
 const customCommandTimeout = 60000;
 const mediumWait = 30000;
-const pluginId = GetPluginId()
+const pluginId = GetPluginId();
 const appId = getAppId();
 
 describe( 'Verify Wondercart accessibility as per capabilities', () => {
 	const cTBAndYithTrue = JSON.stringify( {
-		canAccessAI: true,
-		canAccessHelpCenter: true,
-		canAccessGlobalCTB: true,
-		hasEcomdash: false,
-		hasYithExtended: true,
-		isEcommerce: true,
-		isJarvis: true,
+		"canAccessAI": true,
+		"canAccessHelpCenter": true,
+		"canAccessGlobalCTB": true,
+		"hasEcomdash": false,
+		"hasYithExtended": true,
+		"isEcommerce": true,
+		"isJarvis": true,
 	} );
 
 	const cTBTrueYithFalse = JSON.stringify( {
 		...cTBAndYithTrue,
-		hasYithExtended: false,
-		isEcommerce: false,
+		"hasYithExtended": false,
+		"isEcommerce": false,
 	} );
 
 	before( () => {
@@ -43,21 +43,21 @@ describe( 'Verify Wondercart accessibility as per capabilities', () => {
 			} );
 	} );
 
-	beforeEach(() => {
+	beforeEach( () => {
 		if ( pluginId !== 'bluehost' ) {
 			this.skip();
 		}
 		cy.visit( '/wp-admin/admin.php?page=' + pluginId + '#/home' );
-	});
+	} );
 
-	after(() => {
+	after( () => {
 		cy.exec(
 			`npx wp-env run cli wp option delete _transient_nfd_site_capabilities`,
 			{ failOnNonZeroExit: false }
 		);
-	})
-	
-	it('Verify if Sales and Discounts sub tab is displayed', () => {
+	} );
+
+	it( 'Verify if Sales and Discounts sub tab is displayed', () => {
 		cy.exec(
 			`npx wp-env run cli wp option delete _transient_nfd_site_capabilities`,
 			{ failOnNonZeroExit: false }
@@ -65,7 +65,7 @@ describe( 'Verify Wondercart accessibility as per capabilities', () => {
 		cy.exec(
 			`npx wp-env run cli wp option set _transient_nfd_site_capabilities '${ cTBAndYithTrue }' --format=json`,
 			{ timeout: customCommandTimeout }
-		)
+		);
 		cy.reload();
 		cy.get( `.${ appId }-app-navitem-Store`, {
 			timeout: mediumWait,
@@ -73,11 +73,15 @@ describe( 'Verify Wondercart accessibility as per capabilities', () => {
 		cy.get( `.${ appId }-app-subnavitem` )
 			.contains( 'Sales & Discounts' )
 			.should( 'exist' );
-	})
+	} );
 
 	it( 'Verify Buy Now is shown when canAccessGlobalCTB is true and commerce addon is false', () => {
 		cy.exec(
-			`npx wp-env run cli wp option update _transient_nfd_site_capabilities '${ cTBTrueYithFalse }'`,
+			`npx wp-env run cli wp option delete _transient_nfd_site_capabilities`,
+			{ failOnNonZeroExit: false }
+		);
+		cy.exec(
+			`npx wp-env run cli wp option set _transient_nfd_site_capabilities '${ cTBTrueYithFalse }'`,
 			{ timeout: customCommandTimeout }
 		).then( ( result ) => {
 			cy.log( result.stdout );
@@ -119,9 +123,13 @@ describe( 'Verify Wondercart accessibility as per capabilities', () => {
 
 	it( 'Verify Install Now is shown when customer has ecommerce addon', () => {
 		cy.exec(
-			`npx wp-env run cli wp option update _transient_nfd_site_capabilities '${ cTBAndYithTrue }' --format=json`,
+			`npx wp-env run cli wp option delete _transient_nfd_site_capabilities`,
+			{ failOnNonZeroExit: false }
+		);
+		cy.exec(
+			`npx wp-env run cli wp option set _transient_nfd_site_capabilities '${ cTBAndYithTrue }' --format=json`,
 			{ timeout: customCommandTimeout }
-		)
+		);
 		cy.reload();
 		cy.get( `.${ appId }-app-navitem-Store`, {
 			timeout: mediumWait,
@@ -159,5 +167,5 @@ describe( 'Verify Wondercart accessibility as per capabilities', () => {
 		cy.contains( 'Create campaign', { timeout: mediumWait } ).should(
 			'exist'
 		);
-	});
+	} );
 } );
