fb6466d83cc9cac68c82d957222ecdf0bf0987d6
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-02-22 21:39:37 +0530

    Changes to createa zip

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'a763e8df5c6d2c2d5235');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9f0e068878394c52b5b3');
