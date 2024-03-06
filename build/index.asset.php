e2259cfb6defe0e4dbad77f866cdf8af70fc71f7
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-03-06 14:47:21 +0530

    Change to install when plugin is not activated

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0ac9c008f0917a11427d');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'c72d2dfba3252c7018a4');
