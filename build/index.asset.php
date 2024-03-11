f6ca7be267e4259b879b4d7f75c9998d39d3b32e
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-03-11 12:26:52 +0530

    added fixes for enable and mange in yith features. Name mismatch in `plugin controller` and `plugins` file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '11bfde3e6f52de3d50f6');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '9c6e5dcd37ddbb90fb06');
