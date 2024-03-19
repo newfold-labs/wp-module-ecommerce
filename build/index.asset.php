97cc35010379859b0ba9f79279e59ff76da275fa
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-03-19 11:27:36 +0530

    Added yith docs urls to learn more links

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3a3889b0f19d49100453');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '69f35e99750e633d9e55');
