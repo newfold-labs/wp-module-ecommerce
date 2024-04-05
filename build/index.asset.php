d82c33f58a2de85650638fb7443582819b86ac67
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-04-05 16:02:14 +0530

    addressing PR comments

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8f78fccfb75aa6cc573f');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'eecc12c22373ac174970');
--- src/components/PayPalButton.js
+++ src/components/PayPalButton.js
@@ -4,11 +4,11 @@ const PaypalButton = () => {
   const ppButton = useRef();
 
   useEffect(() => {
-    const bbt = document.querySelector('.yith-btn-paypal');
-    bbt && ppButton.current.appendChild(bbt);
+    const paypalBtn = document.querySelector('.yith-btn-paypal');
+    paypalBtn && ppButton.current.appendChild(paypalBtn);
     return () => {
       const hiddenWrapper = document.querySelector('.yith-ppcp-hidden-button-wrapper');
-      hiddenWrapper && hiddenWrapper.appendChild(bbt);
+      hiddenWrapper && hiddenWrapper.appendChild(paypalBtn);
     };
   }, []);
 
