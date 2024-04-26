60f3b2cb36a323223f840b79f426bbb33f4ffcfb
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-04-26 16:22:16 +0530

    changes added

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'debe7478171a8eedfff4');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0c5dd459660190a1d771');
