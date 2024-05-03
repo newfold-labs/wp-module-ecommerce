a533fe37d1ac143ff5a5f10b1ef26949249f8a52
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-05-03 15:15:45 +0530

    Version update 1.3.30

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,2 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '76e618e019ad98fa7c7c');
-
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0b1e9d8648a860906e55');
