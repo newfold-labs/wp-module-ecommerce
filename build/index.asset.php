29146ca2aa3bcd4e8b9566dccf83eb3ac0ea8f4f
aratidgr8 <aratidgr8@gmail.com>
2024-05-13 18:15:17 +0530

    PRESS0-1266: Non Ecommerce Customers - Store page

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '963b68aa84a0702a9688');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '7b3e79a5a04d1fa119eb');
--- src/components/TransformtoEcommerce.js
+++ src/components/TransformtoEcommerce.js
@@ -7,12 +7,14 @@ export function TransformtoEcommerce() {
            
         
         <div class="nfd-app-section-container nfd-bg-white nfd-w-full nfd-rounded-lg" id="nonecommerce-features-wrapper">
-            <div class="nfd-app-section-content nfd-border nfd-rounded-lg nfd-p-4 nfd-pt-0 nfd-m-8 nfd--mt-2" style={{borderColor: "#E2E8F0"}}>
-                <div className="nfd-flex">
-                    <TransformStore className="nfd-flex nfd-flex-1 nfd-mt-8" />
-                    <div class="nfd-flex nfd-flex-1">
-                        <h1>Transform your store!</h1>
-                        <p>
+            <div class="nfd-app-section-content nfd-border nfd-rounded-lg nfd-p-4 nfd-pt-0 nfd-m-8 nfd--mt-2 nfd-items-start" style={{borderColor: "#E2E8F0"}}>
+                <div className="nfd-flex nfd-flex-wrap nfd-gap-x-10">
+                    <div className="nfd-flex-none nfd-mt-8 nfd-w-40">
+                        <TransformStore />
+                    </div>                    
+                    <div class="nfd-flex-1 nfd-mt-5 nfd-pt-0.5">
+                        <h1 className="nfd-text-2xl nfd-leading-9 nfd-font-medium" style={{color: "#111729"}}>Transform your store!</h1>
+                        <p style={{fontSize: "0.813rem", color: "#4A5567" }} className="nfd-leading-5">
                             Give your online store a boost with our all-in-one eCommerce bundle filled with exclusive plugins and tools designed to help you sell your products and services like a pro.
                         </p>
                     </div>
