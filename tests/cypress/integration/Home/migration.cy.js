f501a70f015bcac40706c6e74aa6b366b22f7d21
krsomayagi <kr.somayagi@newfold.com>
2024-05-15 20:09:30 +0530

    Written separate it blocks for update name server, connect domain and continue with store setup

--- tests/cypress/integration/Home/migration.cy.js
+++ tests/cypress/integration/Home/migration.cy.js
@@ -6,48 +6,50 @@ import { EventsAPI, APIList } from '../wp-module-support/eventsAPIs.cy';
 
 describe( 'Home page', () => {
 	before( () => {
-		//cy.exec(`npx wp-env run cli wp option set showMigrationSteps ${ dataevent }`);
-		//cy.visit( '/wp-admin/admin.php?page=' + pluginId + '#/home' );
+		cy.exec( `npx wp-env run cli wp option set showMigrationSteps "true"` );
 	} );
 
 	beforeEach( () => {
 		cy.visit( '/wp-admin/admin.php?page=' + pluginId + '#/home' );
 	} );
 
-	it( 'Verify Next steps for your site when showMigrationSteps as true', () => {
-		const steps = [
-			'update nameserver clicked',
-			'connect domain to site clicked',
-			'Migrate View Guide Clicked',
-		];
+	it( 'Verify when update nameserver clicked', () => {
+		const steps = [ 'update nameserver clicked' ];
 		cy.get( '.nfd-grid.nfd-gap-4', { timeout: customCommandTimeout } )
 			.scrollIntoView()
 			.should( 'exist' );
 		cy.wait( 5000 );
 		cy.get( '.nfd-grid.nfd-gap-4 ul li a' ).eq( 0 ).click();
-
-		cy.get( '.nfd-help-center', { timeout: customCommandTimeout } ).should(
-			'exist'
-		);
-		//cy.contains('How do I update my nameserver to BH?').should('be.visible');
-		cy.get( '.close-button' ).click();
-
 		EventsAPI( APIList.update_nameserver, pluginId );
 
-		cy.get( '.nfd-grid.nfd-gap-4 ul li a' ).eq( 1 ).click();
 		cy.get( '.nfd-help-center', { timeout: customCommandTimeout } ).should(
 			'exist'
 		);
-		//cy.contains('How do I connect domain to site in BH?').should('be.visible');
+
 		cy.get( '.close-button' ).click();
+	} );
+
+	it( 'Verify when connect domain to site clicked', () => {
+		const steps = [ 'connect domain to site clicked' ];
+		cy.get( '.nfd-grid.nfd-gap-4', { timeout: customCommandTimeout } )
+			.scrollIntoView()
+			.should( 'exist' );
+		cy.wait( 5000 );
+		cy.get( '.nfd-grid.nfd-gap-4 ul li a' ).eq( 1 ).click();
 		EventsAPI( APIList.connect_domain, pluginId );
+		cy.get( '.nfd-help-center', { timeout: customCommandTimeout } ).should(
+			'exist'
+		);
 
-		//cy.get('.nfd-grid.nfd-gap-4 ul li a').eq(2).click();
-		//cy.get( '.nfd-help-center', { timeout: customCommandTimeout } )
+		cy.get( '.close-button' ).click();
+	} );
 
-		//.should( 'exist' );
-		//cy.contains('How do I update my nameserver to BH?').should('be.visible');
-		//cy.get('.close-button').click();
-		//EventsAPI( APIList.store_setup, pluginId );
+	it( 'Verify when continue with store setup clicked', () => {
+		const steps = [ 'continue with store setup clicked' ];
+		cy.get( '.nfd-grid.nfd-gap-4', { timeout: customCommandTimeout } )
+			.scrollIntoView()
+			.should( 'exist' );
+		cy.wait( 5000 );
+		cy.get( '.nfd-grid.nfd-gap-4 ul li a' ).eq( 2 ).click();
 	} );
 } );
