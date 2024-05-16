1ec12a08c76eebf6b034fe1394d2d660ab470f1b
aratidgr8 <aratidgr8@gmail.com>
2024-05-16 16:31:35 +0530

    Resolve conflicts with trunk branch

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< PRESS0-1159
 <?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '1a14649274fcde84a6d6');
-=======
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '0b1e9d8648a860906e55');
->>>>>>> trunk
