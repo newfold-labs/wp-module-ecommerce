91b90e7e273c1ed7fac9311281614d4238393996
manikantakailasa <manikanta.kailasa@gmail.com>
2024-05-24 10:09:20 +0530

    fix ecomdash

--- src/configs/selectors.js
+++ src/configs/selectors.js
@@ -31,8 +31,9 @@ export const wcProductsParser = (type) => (products) => {
 };
 
 export const wcPluginStatusParser = (slug) => (data) => {
+  console.log(data)
   const isInstalled = data?.details?.[slug].status === "active";
-  const isQueueEmpty = data?.queue?.length === 0;
+  const isQueueEmpty = data?.queue[0] ? data?.queue[0]?.length === 0 ? true : false : data?.queue?.length === 0;
   const isInstalling = data?.queue?.includes(slug);
   const isWCActive = data?.details?.woocommerce.status === "active";
   const pluginUrl = data?.details?.[slug].url;
