d04b093b51eaf915b117acc9064341dbb867c495
aratidgr8 <aratidgr8@gmail.com>
2024-07-04 19:10:45 +0530

    Sales Channel page on Ecommerce

---
+++ src/components/SalesChannel.js
@@ -0,0 +1,103 @@
+import { __ } from "@wordpress/i18n";
+import { Button } from "@newfold/ui-component-library";
+import { Section } from "./Section";
+import { ReactComponent as Ecomdash } from "../icons/sales-channel/ecomdash";
+import { ReactComponent as Inventory } from "../icons/sales-channel/inventory-illustration";
+import { ReactComponent as InventoryBrands } from "../icons/sales-channel/inventory-brands";
+import { ReactComponent as OrdersTable } from "../icons/sales-channel/orders-table";
+import { ReactComponent as Amazon } from "../icons/sales-channel/amazon-icon";
+import { ReactComponent as Barcodes } from "../icons/sales-channel/barcodes";
+import { ReactComponent as CustomTemplates } from "../icons/sales-channel/custom-templates";
+import { ReactComponent as ShippingLabels } from "../icons/sales-channel/shipping-labels";
+import { ReactComponent as WarehouseFeeds } from "../icons/sales-channel/warehouse-feeds";
+import { ReactComponent as ListingPresets } from "../icons/sales-channel/listing-presets";
+import { ReactComponent as PurchaseOrders } from "../icons/sales-channel/purchase-orders";
+import { ReactComponent as Accounting } from "../icons/sales-channel/accounting";
+
+export function SalesChannel(){
+    return(
+        <>
+            <Section.Container>
+                <Section.Header title={__("Sales Channels", "wp-module-ecommerce")} subTitle={__("Sell your products everywhere, confidently, with Ecomdash.", "wp-module-ecommerce")} />
+                <Section.Content>
+                    <div className="nfd-flex nfd-flex-wrap">
+                        <h2>{__("Sell your products everywhere, confidently, with Ecomdash.", "wp-module-ecommerce")}</h2>
+                        <p>{__("With ecomdash, you can manage different sales channels all in one place. Simply connect your business accounts from Amazon, Etsy or Walmart  to your ecomdash platform and easily manage products, customer orders and more saving you valuable time.", "wp-module-ecommerce")}​</p>
+                        <Ecomdash />
+                        <Button
+                        id="install-ecomdash"
+                        >
+                            {__("Get Started Now", "wp-module-ecommerce")}
+                        </Button>
+                    </div>
+
+                    <div className="">
+                        <h2>{__("Manage inventory across multiple platforms", "wp-module-ecommerce")}</h2>
+                        <Inventory />
+                        <ul>
+                            <li>{__("Enjoy near real-time inventory updates for an unlimited number of channels with our inventory management software.", "wp-module-ecommerce")}</li>
+                            <li>{__("Manage product levels across multiple warehouses without the fear of overselling.", "wp-module-ecommerce")}</li>
+                            <li>{__("Accurately sync kits, multipacks, and components without any headaches.", "wp-module-ecommerce")}</li>
+                        </ul>
+                        <InventoryBrands />    
+                    </div>
+
+                    <div className="">
+                        <h2>{__("Order Management and Reporting", "wp-module-ecommerce")}</h2>
+                        <p>{__("With Ecomdash you can sit back and relax, knowing that all aspects of order fulfillment are under control. Easily track your top selling items, cost of goods sold, stale inventory and more with Ecomdash’s extensive reporting tools.", "wp-module-ecommerce")}</p>
+                        <OrdersTable />
+                    </div>
+
+                    <div className="">
+                        <h2>{__("Powerful Add-ons", "wp-module-ecommerce")}</h2>
+                        <p>{__("Ecomdash contains a variety of powerful add-ons that will help you handle everything from creating barcodes and inventory templates, to managing warehouse feeds, Fulfillment By Amazon and even accounting.", "wp-module-ecommerce")}</p>
+                        <div className="">
+                            <Amazon />
+                            <h2>{__("Fullfillment By Amazon (FBA)", "wp-module-ecommerce")}</h2>
+                            <p>{__("Auto-export non-Amazon orders to FBA. Import the tracking and pass it to the originating sales channel, marking it as shipped.", "wp-module-ecommerce")}</p>
+                        </div>
+                        <ul>
+                            <li>
+                                <Barcodes />
+                                <span>{__("Barcodes", "wp-module-ecommerce")}</span>
+                                <span>{__("Print barcodes for your products that can be used for labeling and scanning your products", "wp-module-ecommerce")}</span>
+                            </li>
+                            <li>
+                                <CustomTemplates />
+                                <span>{__("Custom Inventory Templates", "wp-module-ecommerce")}</span>
+                                <span>{__("Creating a custom file template allows you to upload and/or export files with product data for thousands of products at once.", "wp-module-ecommerce")}</span>
+                            </li>
+                            <li>
+                                <ShippingLabels />
+                                <span>{__("Shipping Labels", "wp-module-ecommerce")}</span>
+                                <span>{__("Print shipping labels with a preferred carrier right from your ecomdash account and save on postage.", "wp-module-ecommerce")}</span>
+                            </li>
+
+                            <li>
+                                <WarehouseFeeds />
+                                <span>{__("Warehouse Feeds", "wp-module-ecommerce")}</span>
+                                <span>{__("Setup feeds to export your sales orders via email or FTP sites to third parties such as Dropshippers, Suppliers, or 3PL systems.", "wp-module-ecommerce")}</span>
+                            </li>
+                            <li>
+                                <ListingPresets />
+                                <span>{__("Listing Presets", "wp-module-ecommerce")}</span>
+                                <span>{__("Create listing presets to cut down most of the manual data entry needed when creating new listings. ", "wp-module-ecommerce")}</span>
+                            </li>
+                            <li>
+                                <PurchaseOrders />
+                                <span>{__("Purchase Orders", "wp-module-ecommerce")}</span>
+                                <span>{__("Quickly create new orders and have inventory quantities adjusted and synced out to listings as units are received.", "wp-module-ecommerce")}</span>
+                            </li>
+                            <li>
+                                <Accounting />
+                                <span>{__("Accounting", "wp-module-ecommerce")}</span>
+                                <span>{__("Seamlessly communicate your sales and purchase order information directly to your accounting software.", "wp-module-ecommerce")}</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                </Section.Content>
+           </Section.Container> 
+        </>
+    )
+}
