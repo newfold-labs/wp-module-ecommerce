f6f3af3ac54e13d63692e3645dfd8e4fef103d52
aratidgr8 <aratidgr8@gmail.com>
2024-09-13 20:32:04 +0530

    No existing plan view of Homepage - PRESS0-2229

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b11ad9822cad98b6905c');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '8bcd77f9d4138c07dfb4');
