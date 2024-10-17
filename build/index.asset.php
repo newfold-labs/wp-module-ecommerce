db7095621cc83ae7c3b71a1c05370f5724a98f48
Evan Mullins <evanrm@gmail.com>
2024-10-17 17:11:17 -0400

    prep for beta 6
    update version to 1.4.0-beta.6
    and update build files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'b89582e5db2185567ce8');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'fb65f819cfc42d4532da');
