cc394287941ad70a366dbc32353ff59b21528618
Evan Mullins <evanrm@gmail.com>
2024-10-21 11:14:09 -0400

    updates for 1.4.0-beta.8 release

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '4f2a13062d5341748dcd');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '94f4abfe8dc8e6c9707e');
