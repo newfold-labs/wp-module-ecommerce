ce8edcab4ea07630991128a1109ad6b7fd5eeed5
Evan Mullins <evanrm@gmail.com>
2025-01-10 15:01:12 -0500

    update build file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,2 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'c005cfa3f62ea8c3e4db');
-
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '95beac9748ed6ebd5f44');
