9046016da64108beb02794d124de8ccb92eb1ffa
Evan Mullins <evanrm@gmail.com>
2025-01-15 13:24:33 -0500

    revert unnecessary changes

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '73ce8265fc804d74ffbd');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'cb158ecf75343afb99af');
--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -2,10 +2,10 @@
  * Import @wordpress/scripts base webpack config.
  * @see https://github.com/WordPress/gutenberg/tree/master/packages/scripts#extending-the-webpack-config
  */
-const wpScriptsConfig = require( '@wordpress/scripts/config/webpack.config' );
+const wpScriptsConfig = require('@wordpress/scripts/config/webpack.config');
 
 wpScriptsConfig.output.library = {
-	type: 'commonjs',
-};
+    type: 'commonjs',
+}
 
 module.exports = wpScriptsConfig;
