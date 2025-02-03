389c0146376cd240427a6ea615ffda7de636112c
Evan Mullins <evanrm@gmail.com>
2025-02-03 18:30:27 -0500

    update js-utility-ui-analytics to npmjs version

--- src/sdk/analytics.js
+++ src/sdk/analytics.js
@@ -1,8 +1,9 @@
 import {
   HiiveAnalytics,
   HiiveEvent,
-} from "@newfold-labs/js-utility-ui-analytics";
+} from "@newfold/js-utility-ui-analytics";
 import { NewfoldRuntime } from "@newfold/wp-module-runtime";
+import data from "@wordpress/data"; // added to keep dependency for js-utility-ui-analytics
 
 export const AnalyticsSdk = {
   track(category, action, value) {
