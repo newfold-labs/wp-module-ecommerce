7116c9186c9e4dc6cd70bd33edc9fecb8f538068
Evan Mullins <evanrm@gmail.com>
2025-02-04 17:49:21 -0500

    also import api-fetch and wrap in dependencies object
    import as wpX to namespace values

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'c32db55d2154248d2e88');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '31b06a75b450ead36c57');
