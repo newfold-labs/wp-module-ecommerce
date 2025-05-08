a671a893470a6e0b12f22509f8a601371cdb9264
ajayadav09 <ajayadav09@gmail.com>
2025-05-08 18:24:43 +0530

    PRESS6-340 | reverted woocommerce coupon field changes
    Reverted changes made in this PR https://github.com/newfold-labs/wp-module-ecommerce/pull/182/files

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '19c4592b547ac047f3ef');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-data', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '882080872b2953a805d9');
