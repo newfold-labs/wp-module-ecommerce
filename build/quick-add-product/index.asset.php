637fe9c30019e825583a0d0f153e236972d61d16
Francesco Licandro <francesco.licandro@yithemes.com>
2025-07-03 14:14:37 +0200

    build scripts

--- build/quick-add-product/index.asset.php
+++ build/quick-add-product/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '5c64643e6f5651776ae0');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '2f49220cbb2a75917e8c');
