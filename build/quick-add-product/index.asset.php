4d4380e7791b199d9070d7f8a19c03cb8a068133
Francesco Licandro <francesco.licandro@yithemes.com>
2025-09-15 15:39:20 +0200

    improve responsive

--- build/quick-add-product/index.asset.php
+++ build/quick-add-product/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '76a6aa659dd29e200d6c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'c7f3c61d3e0b57bb4128');
