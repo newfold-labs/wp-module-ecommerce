862ce60229892173934686b3d028954c185d2b07
Evan Mullins <evanrm@gmail.com>
2025-10-02 09:31:47 -0400

    check for target and parents having quick add trigger

--- build/quick-add-product/index.asset.php
+++ build/quick-add-product/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '42c679609a7c208e1a79');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'bdb988b4038ae8e43a3d');
